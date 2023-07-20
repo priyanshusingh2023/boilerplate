4d1ded3ff9eb699794135b3c38f38c4bc6483b53
priyanshusingh2023 <139940149+priyanshusingh2023@users.noreply.github.com>
2023-07-20 09:43:21 +0000

    Dockerhub

--- src/main/java/com/stackroute/HealthMonitoring/HealthMonitoringApplication.java
+++ src/main/java/com/stackroute/HealthMonitoring/HealthMonitoringApplication.java
@@ -1,11 +1,16 @@
-package com.stackroute.HealthMonitoring;
+// This code creates an instance of the HealthMonitoringApplication class and runs it.
 
+package com.stackroute.HealthMonitoring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class HealthMonitoringApplication {
+
+	// Main method to run the application
 	public static void main(String[] args) {
 		SpringApplication.run(HealthMonitoringApplication.class, args);
 	}
+
 }
